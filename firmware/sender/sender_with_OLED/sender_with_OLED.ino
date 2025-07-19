1301bd5e479203a49d798cae55a25a3c3b8f0f3d
Kelia Gjurgjaj <keliagjurgjaj1@gmail.com>
2025-07-18 21:48:44 -0400

    Add firmware code and field-tests report

---
+++ firmware/sender/sender_with_OLED/sender_with_OLED.ino
@@ -0,0 +1,237 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-anemometer-wind-speed-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files. The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Softwar
+*********/
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://RandomNerdTutorials.com/esp32-lora-rfm95-transceiver-arduino-ide/
+*********/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//define the pins used by the transceiver module **LORA
+#define ss 5
+#define rst 26
+#define dio0 27
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME680 bme; // I2C
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Constants (Change the following variables if needed)
+const int anemometerPin = 34;  // GPIO pin connected to anemometer (analog pin)
+const float minVoltage = 0.4;  // Voltage corresponding to 0 m/s
+const float maxVoltage = 2.0;  // Voltage corresponding to 32.4 m/s (max speed) (when using voltage divider)
+const float maxWindSpeed = 32.4; // Maximum wind speed in m/s
+
+// Conversion factors
+const float mps_to_kmh = 3.6;   // 1 m/s = 3.6 km/h
+const float mps_to_mph = 2.23694; // 1 m/s = 2.23694 mph
+
+// LoRa Packet Sender **
+int counter = 0;
+
+void setup() {
+
+  Wire.begin(21, 22);  // Use custom I2C pins if needed
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Starting...");
+  display.display();
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  Serial.begin(115200); 
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+
+
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //868E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(915E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+
+  // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setTxPower(17);
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+
+  Serial.println(F("BME680 async test"));
+  if (!bme.begin()) {
+    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms 
+}
+
+void loop() {
+  
+  // Tell BME680 to begin measurement.
+  //Serial.println("INSIDE LOOP");
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading :("));
+    return;
+  }  
+  delay(50);
+  
+  
+  // Obtain measurement results from BME680. Note that this operation isn't
+  // instantaneous even if milli() >= endTime due to I2C/SPI latency.
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading :("));
+    return;
+  }
+
+  
+  // Read analog value from anemometer (ADC value between 0-4095 on ESP32 for 0-3.3V)
+  int adcValue = analogRead(anemometerPin);
+  
+  // Convert ADC value to voltage (ESP32 ADC range is 0-3.3V)
+  float voltage = (adcValue / 4095.00) * 3.3;
+  
+  // Ensure the voltage is within the anemometer operating range
+  if (voltage < minVoltage) {
+    voltage = minVoltage;
+  } else if (voltage > maxVoltage) {
+    voltage = maxVoltage;
+  }
+  
+  // Map the voltage to wind speed
+  float windSpeed_mps = ((voltage - minVoltage) / (maxVoltage - minVoltage)) * maxWindSpeed;
+
+  // Convert wind speed to km/h and mph
+  float windSpeed_kmh = windSpeed_mps * mps_to_kmh;
+  float windSpeed_mph = windSpeed_mps * mps_to_mph;
+
+  // Print wind speed
+  Serial.print(" X:");
+  Serial.print(windSpeed_mps);
+  Serial.print(" Y:");
+  Serial.print(windSpeed_kmh);
+  Serial.print(" Z:");
+  Serial.print(windSpeed_mph);
+  
+  delay(1000);
+  
+  Serial.print(" T:");
+  Serial.print(bme.temperature);
+
+  Serial.print(" P:");
+  Serial.print(bme.pressure/100.0);
+
+  Serial.print(" H:");
+  Serial.print(bme.humidity);
+
+  Serial.print(" G:");
+  Serial.print(bme.gas_resistance / 1000.0);
+/*
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+  */
+  Serial.println();
+  delay(1000);
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // --- Show "Sending..." on screen ---
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.println("Sending packet...");
+  display.println(counter);
+  display.display();
+  delay(800);  // show it for half a second
+  //display.clearDisplay();
+  
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("#");
+  LoRa.print(counter);
+  // Print wind speed and environmental data
+  LoRa.print(" ID:B X:" + String(windSpeed_mps) 
+             + " Y:" + String(windSpeed_kmh) 
+             + " Z:" + String(windSpeed_mph) 
+             + " T:" + String(bme.temperature)
+             + " P:" + String(bme.pressure / 100.0)
+             + " H:" + String(bme.humidity)
+             + " G:" + String(bme.gas_resistance / 1000.0));
+
+  LoRa.endPacket();
+
+  // --- Optional: change to "Sent!" then clear ---
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor((SCREEN_WIDTH - 6 * 5) / 2, 0);  // roughly center
+  display.println("Sent!");
+  display.display();
+  delay(800); // show "Sent!" for a moment
+
+  // Now show the selected sensor data
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+
+  display.print("Temp: ");
+  display.print(bme.temperature, 1);
+  display.println(" C");
+
+  display.print("Hum : ");
+  display.print(bme.humidity, 1);
+  display.println(" %");
+
+  display.print("Gas : ");
+  display.print(bme.gas_resistance / 1000.0, 1);
+  display.println(" kOhm");
+
+  display.print("Wind: ");
+  display.print(windSpeed_mph, 1);
+  display.println(" mph");
+  display.display();
+
+  counter++;
+  delay(1000);
+  delay(7000); // every 10 seconds
+  //delay(30000);
+
+}
---
+++ receiver/receiver_with_display/receiver_with_display.ino
@@ -0,0 +1,206 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://RandomNerdTutorials.com/esp32-lora-rfm95-transceiver-arduino-ide/
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+// WiFi / Google Sheets
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SharpMem.h>
+
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 26
+#define dio0 27
+
+#define SHARP_SCK   14  // CLK
+#define SHARP_MOSI  13  // MOSI
+#define SHARP_SS    15  // CS
+#define SHARP_DISP  25  // Display enable pin
+#define SHARP_VCOM  4   // VCOM toggle pin
+#define BLACK 0
+#define WHITE 1
+
+Adafruit_SharpMem display(SHARP_SCK, SHARP_MOSI, SHARP_SS, 400, 240);
+
+// WiFi Stuff below
+
+// Replace with your network credentials
+const char* ssid = "Starlink";
+const char* password = "fredfontaine";
+const char* serverAddress = "10.5.1.53";
+const int serverPort = 80;
+
+// Google Apps Script Web App URL
+const char* serverName = "https://script.google.com/macros/s/AKfycbywnSWLs8_LqM3vRi_-mjHhK1A9MMaT3hCivDwBCKAHPst_-MhH9RBAlrO_p4jzx8mIag/exec";
+
+// Initialize WiFi and NTP client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 7200, 60000); // Update every 60 seconds
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //868E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(915E6)) {
+    Serial.println(".");
+    delay(500);
+
+  pinMode(SHARP_DISP, OUTPUT);
+  digitalWrite(SHARP_DISP, HIGH);
+  pinMode(SHARP_VCOM, OUTPUT);
+
+  display.begin();
+  display.clearDisplay();
+  display.refresh();  
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setTxPower(20); //CHANGE THIS DURING TESTING ***************************************************************
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+
+  // Setup WiFi connectionl;'
+  ///////5
+  delay(100);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
+
+  // Initialize NTP client
+  timeClient.begin();
+  timeClient.update();
+}
+
+void loop() {
+  // Toggle EXTCOMIN for Sharp display every second
+  static unsigned long lastVCOM = 0;
+  static bool vcomState = false;
+  if (millis() - lastVCOM >= 1000) {
+    vcomState = !vcomState;
+    digitalWrite(SHARP_VCOM, vcomState);
+    lastVCOM = millis();
+  }
+
+      // Display on Sharp Memory LCD
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(BLACK);
+    display.setCursor(0, 0);
+
+    display.print("Starting... ");
+        display.refresh();
+
+  // Check for LoRa packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // Read LoRa message into input
+    String input = "";
+    while (LoRa.available()) {
+      input += (char)LoRa.read();
+    }
+
+    Serial.print("Received: ");
+    Serial.println(input);
+    float rssi = LoRa.packetRssi();
+    Serial.print("RSSI: ");
+    Serial.println(rssi);
+
+    // Parse values
+    int num = input.substring(input.indexOf("#") + 1, input.indexOf(" ID:")).toInt();
+    String id = input.substring(input.indexOf("ID:") + 3, input.indexOf(" X:"));
+    float windSpeed = input.substring(input.indexOf("X:") + 2, input.indexOf(" Y:")).toFloat();
+    float windSpeedKm = input.substring(input.indexOf("Y:") + 2, input.indexOf(" Z:")).toFloat();
+    float windSpeedMph = input.substring(input.indexOf("Z:") + 2, input.indexOf(" T:")).toFloat();
+    float temperature = input.substring(input.indexOf("T:") + 2, input.indexOf(" P:")).toFloat();
+    float pressure = input.substring(input.indexOf("P:") + 2, input.indexOf(" H:")).toFloat();
+    float humidity = input.substring(input.indexOf("H:") + 2, input.indexOf(" G:")).toFloat();
+    float gas = input.substring(input.indexOf("G:") + 2).toFloat();
+
+    // Display on Sharp Memory LCD
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(BLACK);
+    display.setCursor(0, 0);
+
+    display.print("Temp: ");
+    display.print(temperature, 1);
+    display.println(" C");
+
+    display.print("Hum:  ");
+    display.print(humidity, 1);
+    display.println(" %");
+
+    display.print("Gas:  ");
+    display.print(gas, 1);
+    display.println(" kOhm");
+
+    display.print("Wind: ");
+    display.print(windSpeedMph, 1);
+    display.println(" mph");
+
+    display.refresh();
+
+    // Send to Google Sheets
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      http.begin(serverName);
+      http.addHeader("Content-Type", "application/json");
+
+      timeClient.update();
+      String formattedTime = timeClient.getFormattedTime();
+
+      String jsonData = "{";
+      jsonData += "\"method\":\"append\",";
+      jsonData += "\"timestamp\":\"" + formattedTime + "\",";
+      jsonData += "\"count\":\"" + String(num) + "\",";
+      jsonData += "\"id\":\"" + id + "\",";
+      jsonData += "\"temp\":\"" + String(temperature) + "\",";
+      jsonData += "\"hum\":\"" + String(humidity) + "\",";
+      jsonData += "\"pres\":\"" + String(pressure) + "\",";
+      jsonData += "\"gas\":\"" + String(gas) + "\",";
+      jsonData += "\"wkm\":\"" + String(windSpeedKm) + "\",";
+      jsonData += "\"wmps\":\"" + String(windSpeed) + "\",";
+      jsonData += "\"wmph\":\"" + String(windSpeedMph) + "\",";
+      jsonData += "\"rssi\":\"" + String(rssi) + "\"}";
+
+      Serial.println("Sending JSON Data: " + jsonData);
+
+      int httpResponseCode = http.POST(jsonData);
+      if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+      } else {
+        Serial.println("Error on sending POST: " + String(httpResponseCode));
+      }
+
+      http.end();
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+
+    Serial.println("done with wifi stuff");
+  }
+}
